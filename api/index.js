ce4e4a38ce3f76ff44e9cff351b3e5af8ef05f82
Thang Kieu <kqthang1505@gmail.com>
2025-12-26 13:37:21 +0800

    chore: add referal

--- api/index.js
+++ api/index.js
@@ -62,6 +62,7 @@ async function scrapeAndSend(chatId, targetUrl) {
                     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                     'Accept-Language': 'en-US,en;q=0.5',
+                    'Referer': targetUrl
                 }
             }
         );
