7d23070aa999fd19ae7a44e1e76ebb1a295fe88b
Thang Kieu <kqthang1505@gmail.com>
2025-12-26 13:42:40 +0800

    chore: update headers

--- api/index.js
+++ api/index.js
@@ -55,17 +55,14 @@ async function handleScrapeRequest(ctx) {
 
 // Move the heavy logic to a separate async function
 async function scrapeAndSend(chatId, targetUrl) {
+    const headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Referer': targetUrl
+    };
+
     try {
-        const { data } = await axios.get(targetUrl,
-            {
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
-                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-                    'Accept-Language': 'en-US,en;q=0.5',
-                    'Referer': targetUrl
-                }
-            }
-        );
+        const { data } = await axios.get(targetUrl, { headers });
         const $ = cheerio.load(data);
         const imageElements = $('.entry-content img').get();
 
